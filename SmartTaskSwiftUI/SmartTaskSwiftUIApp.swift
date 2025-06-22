a7475eed17e0e149ecd2d7e025b68423e7e7147c
MQamarInam <qamarinam028@gmail.com>
2025-06-22 13:05:03 +0500

    Initial Commit

---
+++ SmartTaskSwiftUI/SmartTaskSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartTaskSwiftUIApp.swift
+//  SmartTaskSwiftUI
+//
+//  Created by Qaim's Macbook  on 22/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct SmartTaskSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

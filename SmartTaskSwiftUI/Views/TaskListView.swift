2317e13a9346989e8caa512d3b627f9ff63178fa
MQamarInam <qamarinam028@gmail.com>
2025-06-22 23:55:41 +0500

    Created TaskListView to show List of Tasks

---
+++ SmartTaskSwiftUI/Views/TaskListView.swift
@@ -0,0 +1,18 @@
+//
+//  TaskListView.swift
+//  SmartTaskSwiftUI
+//
+//  Created by Qaim's Macbook  on 22/06/2025.
+//
+
+import SwiftUI
+
+struct TaskListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskListView()
+}
